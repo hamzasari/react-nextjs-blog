59b7d01ac2432b0ef23cde968b35a9110d0311af
Hamza SARI <hamza.sari@gmail.com>
2022-08-20 19:22:17 +0300

    await keyword is added because of the getPostData function is changed to an async function.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -12,7 +12,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params }) => {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
 
   return {
     props: {
