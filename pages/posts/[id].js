522608ced07e5aff9b9b8ca9b373fd6d44804cdb
Hamza SARI <hamza.sari@gmail.com>
2022-08-20 19:31:37 +0300

    styles are added to post page.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 
 import Layout from '../../components/Layout';
+import Date from '../../components/Date';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
+
+import utilStyles from '../../styles/utils.module.css';
 
 export const getStaticPaths = async () => {
   const paths = getAllPostIds();
@@ -30,13 +33,13 @@ const Post = ({ postData }) => {
         <title>{postData.title}</title>
       </Head>
 
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 };
